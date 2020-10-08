67b31b47ed65177bd51827d1fb11640ca35d1602
soaelee <soae0923@gmail.com>
2020-10-08 15:51:14 +0900

    HTML/CSS: main.html(home, about)

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-#main service reservation search
+# main service reservation search
+
 
 @app.route("/")
 def main():
     return render_template('main.html')
 
+
 @app.route("/service")
 def service():
     return render_template('service.html')
+
 
 @app.route("/reservation")
 def reservation():
     return render_template('reservation.html')
 
+
 @app.route("/search")
 def search():
     return
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+d
