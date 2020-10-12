ccf73f94506f6a06ac3a6afedc4b3b00f3113bb1
RealSong <july2day@gmail.com>
2020-10-12 08:02:06 +0000

    main.py modify

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mariadb
+import sys
 
 app = Flask(__name__)
 # main service reservation search
 
+def get_conn():
+    conn = mariadb.connect(
+            user="root",
+            password="000000",
+            host="193.123.233.236",
+            port=3306,
+            database="petland"
+            )
+    return conn
 
 @app.route("/")
 def main():
