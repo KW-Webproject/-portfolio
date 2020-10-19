f4593978c90111f4340929407b3b6955ba01f369
RealSong <july2day@gmail.com>
2020-10-19 19:21:33 +0900

    201019 - main.py 메소드 추가

--- main.py
+++ main.py
@@ -127,6 +127,23 @@ def receive_form():
         err = "펫시터가 없음"
         return render_template("reservation.html", err=err)
 
+# 결제하기 작동시 /reservation2로 form을 보내고 reservation.html 로 이동
+
+
+@app.route("/reservation2", methods=['POST'])
+def reservation2():
+    name = request.form['name']
+    phone = request.form['phone']
+    pet = request.form['pet']
+    service = request.form.getlist('service')
+    date = request.form['date']
+    time = request.form.getlist('time')
+    postcode = request.form['postcode']
+    roadaddress = request.form['roadaddress']
+    detailaddress = request.form['detailaddress']
+
+    render_template("reservation.html")
+
 
 @app.route("/reservation_check")
 def reservation_check():
@@ -138,13 +155,19 @@ def check_submit():
     name = request.form['name']
     phone = request.form['phone']
     print(name, phone)
+    print(type(name))
+    print(type(phone))
+    # 체크 값이 있을 시
+    # user_info[p_id, name, phone, pet, service(리스트), p_date, r_time(리스트), r_p_id ]
     user_info = check_phone(phone, name)
     if len(user_info) == 0:
         err = "예약정보가 없습니다."
         return render_template("reservation_check.html", err=err)
     else:
         print(user_info)
-        return "조회가 되었다"
+        return render_template("reservation_check2.html",
+                               id=user_info[0], name=user_info[1], pet=user_info[3], service=user_info[4], date=user_info[5],
+                               time=user_info[6], petsitter="냉무")
 
 # 입력된 주소로 펫시터가 있는지 체크한다.
 
@@ -154,39 +177,60 @@ def check_add(search_local):
     sql = """
         SELECT p_id, p_name FROM pet_sitter WHERE p_local = "{}"
     """.format(search_local)
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute(sql)
-    r_id = cur.fetchone()
-    if r_id == None:
-        return 0
-    else:
-        return r_id
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        r_id = cur.fetchone()
+    except mariadb.Error as e:
+        print("ERR : {}".format(e))
+    finally:
+        if conn:
+            conn.close()
+        if r_id == None:
+            return 0
+        else:
+            return r_id
 
 
+# 조회가 되었을 시 user_info에 값을 전달한다.
 def check_phone(user_phone, user_name):
     user_info = []
     sql = """
         SELECT * FROM reservation WHERE phone = "{}" AND name= "{}"
         """.format(user_phone, user_name)
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute(sql)
-    for i in cur:
-        user_info += i
-    print(user_info)
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        for i in cur:
+            user_info += i
+        print(user_info)
+    except mariadb.Error as e:
+        print("ERR : {}".format(e))
+    finally:
+        if conn:
+            conn.close()
     return user_info
 
-# 결제하기 했을 때 예약자의 데이터를 저장
 
-
+# 결제하기 했을 때 예약자의 데이터를 저장, 내일 처리
 def payment_save():
-    return
-
-# 조회가 되었을 시
-
-
-def inquiry_return():
+    err = ""
+    sql = """INSERT INTO reservation
+        (name, phone, pet, service, p_date, r_time)
+        values ("{}","{}","{}","{}","{}","{}")
+    """.format(name, phone, pet, service, p_date, r_time)
+    print(sql)
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+    except mariadb.Error as err:
+        print("ERROR: {}".format(err))
+    if conn:
+        conn.close()
     return
 
 
