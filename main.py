92aeb913593c00c30a3712a06aa8595c8779669d
RealSong <july2day@gmail.com>
2020-10-20 15:14:34 +0900

    201020 - main.py reservation2.html 결제하기 수정

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import mariadb
 import sys
 
 app = Flask(__name__)
-
-
-# class User():
+# 예약자 정보를 결제하기까지 저장하기위해 전역변수 사용
+reservation_info = []
 
 
 def get_conn():
@@ -15,58 +14,6 @@ def get_conn():
                            port=3306,
                            database="petland")
     return conn
-
-# conn = get_conn()
-# cur = conn.cursor()
-
-# @app.route("/form", methods=['POST'])
-# def form():
-#     if request.method == 'POST':
-#         reservation_form = request.form
-#         name = reservation_form['name']
-#         phone = reservation_form['phone']
-#         pet = reservation_form['pet']
-#         service = reservation_form['service']
-#         p_date = reservation_form['date']
-#         r_time = reservation_form['time']
-
-#         sql = """INSERT INTO reservation
-#             (name, phone, pet, service, p_date, r_time)
-#             values ("{}","{}","{}","{}","{}","{}")
-#         """.format(name, phone, pet, service, p_date, r_time)
-#         print(sql)
-#         cur.execute(sql)
-
-#         conn.commit()
-#         conn.close()
-#         return "예약이 완료되었습니다."
-#     return render_template("Form.html")
-# @app.route("/test")
-# def test():
-#     sql = "SELECT p_name, p_date FROM pet_sitter"
-#     result = ""
-
-#     try:
-#         conn = get_conn()
-#         cur = conn.cursor()
-#         cur.execute(
-#                 sql
-#                 )
-#         result += '<ul>'
-#         for(p_name, p_date) in cur:
-#                 result += """
-#                     <li>이름 = {}</li>
-#                     <li>날짜 = {}</li>
-#                     """.format(p_name, p_date)
-#         result += '</ul>'
-
-#     except mariadb.Error:
-#         result = "펫시터 없음"
-#         sys.exit(1)
-#     finally:
-#         if conn:
-#             conn.close()
-#     return result
 
 
 @app.route("/")
@@ -111,8 +58,10 @@ def receive_form():
     formData.append(postcode)
     formData.append(roadaddress)
     formData.append(detailaddress)
-    # formData.append(name, phone, pet, service)
     print(formData)
+    global reservation_info
+    reservation_info = formData
+    print("전역변수 : ", reservation_info)
 
     # 지역(구)를 먼저 빼와서 DB와 비교
     roadaddress = roadaddress.split()
@@ -120,6 +69,8 @@ def receive_form():
     str_addr = roadaddress[1]
     # 펫시터가 존재하면 p_id와 p_name을 저장
     check_sitter = check_add(str_addr)
+    reservation_info.append(check_sitter[0])
+    print(reservation_info)
     print(check_sitter)
     if check_sitter != 0:
         return render_template("reservation2.html",
@@ -132,20 +83,14 @@ def receive_form():
 
 # 결제하기 작동시 /reservation2로 form을 보내고 reservation.html 로 이동
 
+# 결제하기 눌렀을시 db에 데이터 입력
+
 
 @app.route("/reservation2", methods=['POST'])
 def reservation2():
-    name = request.form['name']
-    phone = request.form['phone']
-    pet = request.form['pet']
-    service = request.form.getlist('service')
-    date = request.form['date']
-    time = request.form.getlist('time')
-    postcode = request.form['postcode']
-    roadaddress = request.form['roadaddress']
-    detailaddress = request.form['detailaddress']
-
-    render_template("reservation.html")
+    payment_save(reservation_info)
+    # reservation_info = ""
+    return render_template("reservation.html")
 
 
 @app.route("/reservation_check")
@@ -157,9 +102,6 @@ def reservation_check():
 def check_submit():
     name = request.form['name']
     phone = request.form['phone']
-    print(name, phone)
-    print(type(name))
-    print(type(phone))
     # 체크 값이 있을 시
     # user_info[p_id, name, phone, pet, service(리스트), p_date, r_time(리스트), r_p_id ]
     user_info = check_phone(phone, name)
@@ -178,7 +120,7 @@ def check_submit():
 def check_add(search_local):
     r_id = ""
     sql = """
-        SELECT p_id, p_name FROM pet_sitter WHERE p_local = "{}"
+        SELECT p_id, p_name FROM pet_sitter WHERE p_local = "{}" AND possible = 0
     """.format(search_local)
     try:
         conn = get_conn()
@@ -202,21 +144,23 @@ def check_phone(user_phone, user_name):
     sql = """
         SELECT * FROM reservation WHERE phone = "{}" AND name= "{}"
         """.format(user_phone, user_name)
+    print(sql)
     try:
         conn = get_conn()
         cur = conn.cursor()
         cur.execute(sql)
         for i in cur:
             user_info += i
+        print(user_info)
         # pet_sitter 테이블에서 possible colum을 빼온다
 
-        sql = """
-            SELECT
-        """
-        conn = get_conn()
-        cur = conn.cursor()
-        cur.execute(sql)
-        print(user_info)
+        # sql = """
+        #     SELECT
+        # """
+        # conn = get_conn()
+        # cur = conn.cursor()
+        # cur.execute(sql)
+        # print(user_info)
     except mariadb.Error as e:
         print("ERR : {}".format(e))
     finally:
@@ -225,24 +169,38 @@ def check_phone(user_phone, user_name):
     return user_info
 
 
-# 결제하기 했을 때 예약자의 데이터를 저장, 내일 처리
-def payment_save():
+# 결제하기 했을 때 예약자의 데이터를 저장
+def payment_save(info):
+    # 예약정보를 리스트형식으로 가지고있다. 최종 결제시 db에 입력
+    # name, phone, pet, service, date, time, postcode, roadaddress, detailaddress
+    full_address = ""
+    print(info)
+    print(len(info))
+    full_address = info[6] + " " + info[7] + " " + info[8]
+    print(full_address)
     err = ""
     sql = """INSERT INTO reservation
-        (name, phone, pet, service, p_date, r_time)
-        values ("{}","{}","{}","{}","{}","{}")
-    """.format(name, phone, pet, service, p_date, r_time)
+        (name, phone, pet, service, r_date, r_time, address)
+        values ("{}","{}","{}","{}","{}","{}", "{}")
+    """.format(info[0], info[1], info[2], info[3], info[4], info[5], full_address)
     print(sql)
+    # 펫시터 예약시 possible 업데이트
+
+    sql2 = """
+        UPDATE pet_sitter SET possible=1 WHERE p_id = {}
+    """.format(info[9])
     try:
         conn = get_conn()
         cur = conn.cursor()
         cur.execute(sql)
         conn.commit()
+        cur.execute(sql2)
+        conn.commit()
     except mariadb.Error as err:
         print("ERROR: {}".format(err))
-    if conn:
-        conn.close()
-    return
+    finally:
+        if conn:
+            conn.close()
 
 
 if __name__ == "__main__":
