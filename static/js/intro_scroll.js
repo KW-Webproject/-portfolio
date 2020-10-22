aeaf97cfe47f1e2e6965ffccb5f4614b6c014d2b
soaelee <soae0923@gmail.com>
2020-10-22 18:22:02 +0900

    반응형웹 + 섹션추가

--- static/js/intro_scroll.js
+++ static/js/intro_scroll.js
@@ -2,17 +2,24 @@ const menu = document.querySelector(".nav__menu");
 let selected = document.querySelector(".selected");
 
 
-const home__height1 = document.querySelector("#home").getBoundingClientRect().height;
-const home__height2 = document.querySelector("#responsive").getBoundingClientRect().height;
-const skill_top = document.querySelector("#skills").getBoundingClientRect().top;
+const home = document.querySelector("#home").getBoundingClientRect().height;
+const service_height = document.querySelector("#service").getBoundingClientRect().height;
 const skill_height = document.querySelector("#skills").getBoundingClientRect().height;
-const team_top = document.querySelector("#team").getBoundingClientRect().top;
 const team_height = document.querySelector("#team").getBoundingClientRect().height;
 
-const home = home__height1 + home__height2;
-const skill = home + skill_height;
+const service = home + service_height;
+const skill = service + skill_height;
 const team = skill + team_height;
 
+const toggle = document.querySelector(".header__toggle");
+const nav = document.querySelector(".header__nav");
+
+
+toggle.addEventListener("click", (e) => {
+    console.log(e);
+    toggle.classList.toggle("open");
+    nav.classList.toggle("open");
+})
 menu.addEventListener("click", (event) => {
     const target = event.target;
     const link = target.dataset.link;
@@ -29,7 +36,11 @@ window.addEventListener("scroll", () => {
         selected.classList.remove("selected");
         document.querySelector(".about").classList.add("selected");
         selected = document.querySelector(".about")
-    } else if (home < window.scrollY && window.scrollY < skill) {
+    } else if (home < window.scrollY && window.scrollY < service) {
+        selected.classList.remove("selected");
+        document.querySelector(".service").classList.add("selected");
+        selected = document.querySelector(".service")
+    } else if (service < window.scrollY && window.scrollY < skill) {
         selected.classList.remove("selected");
         document.querySelector(".skills").classList.add("selected");
         selected = document.querySelector(".skills")
